90f5fc36a24e8731eb7c470144de350184314eb2
John Postlethwait <john.postlethwait@nebula.com>
2013-08-23 19:14:03 +0000

    Updating package to be more up-to-date with the current state of the dependencies.

--- test/sync_test.js
+++ test/sync_test.js
@@ -4,7 +4,7 @@ var Sync = require('../lib/sync');
 
 describe('Sync', function() {
     var backend, callback, handle, sync;
-    
+
     describe('#handle', function() {
         beforeEach(function() {
             backend = {
@@ -14,7 +14,7 @@ describe('Sync', function() {
                     } else {
                         res.end('foo');
                     }
-                    
+
                     // Should be ignored
                     res.end('baz');
                     res.error('qux');
@@ -24,42 +24,42 @@ describe('Sync', function() {
             handle = sinon.spy(backend, 'handle');
             sync = new Sync('backend', 'socket');
         });
-        
+
         it('should delegate to the backend', function() {
             sync.handle(backend, {}, callback);
-            
+
             assert.ok(handle.calledOnce);
         });
-        
+
         it('should merge `backend` and `socket` into the request object', function() {
             sync.handle(backend, { foo: 'bar' }, callback);
-            
+
             var req = handle.getCall(0).args[0];
-            
+
             assert.equal(req.backend, 'backend');
             assert.equal(req.socket, 'socket');
             assert.equal(req.foo, 'bar');
         });
-        
+
         it('should pass result to callback', function() {
             sync.handle(backend, {}, callback);
-            
+
             var call = callback.getCall(0);
             var err = call.args[0];
             var result = call.args[1];
-            
+
             assert.ok(callback.called);
             assert.equal(err, null);
             assert.equal(result, 'foo');
         });
-        
+
         it('should pass error to callback', function() {
             sync.handle(backend, { error: true }, callback);
-            
+
             var call = callback.getCall(0);
             var err = call.args[0];
             var result = call.args[1];
-            
+
             assert.ok(callback.called);
             assert.ok(err);
             assert.ok(err.error);
